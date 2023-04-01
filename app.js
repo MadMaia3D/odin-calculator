18cf88f11d1192b30bbc988c4c74308eb2df8a1c
MadMaia3D <33674747+MadMaia3D@users.noreply.github.com>
2023-04-01 15:37:22 -0300

    add: function to compute the selected operator

--- app.js
+++ app.js
@@ -13,3 +13,9 @@ function multiply(number1, number2) {
 function divide(number1, number2) {
     return number1 / number2;
 }
+
+function operate(operator, number1, number2) {
+    const operations = { "+": add, "-": subtract, "*": multiply, "/": divide };
+    const result = operations[operator](Number(number1), Number(number2));
+    return result;
+}
