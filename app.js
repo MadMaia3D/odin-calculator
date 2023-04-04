2b6eb7b13cdc8acb80a3206c2b38d5a564601f1e
MadMaia3D <33674747+MadMaia3D@users.noreply.github.com>
2023-04-04 11:32:49 -0300

    add: digit and clear buttons base functionality

--- app.js
+++ app.js
@@ -19,3 +19,28 @@ function operate(operator, number1, number2) {
     const result = operations[operator](Number(number1), Number(number2));
     return result;
 }
+
+let first_input = "";
+
+const displayCurrentInput = document.querySelector(".display .current-input");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    if (button.classList.contains("digit")) {
+        button.addEventListener("click", addDigit);
+    }
+
+    if (button.classList.contains("clear")) {
+        button.addEventListener("click", clear);
+    }
+});
+
+function addDigit(event) {
+    first_input += event.target.dataset.number;
+    displayCurrentInput.textContent = first_input;
+}
+
+function clear() {
+    first_input = "";
+    displayCurrentInput.textContent = "0";
+}
