6f3685fa6b489e3caca84843a11a113687b5faf5
MadMaia3D <33674747+MadMaia3D@users.noreply.github.com>
2023-04-15 00:00:42 -0300

    add: number and operator buttons functionality

--- app.js
+++ app.js
@@ -1,35 +1,82 @@
 class Calculator {
     constructor() {
         this.currentInput = "";
+        this.inputs = [];
+
         this.numberButtons = document.querySelectorAll(".number");
+        this.operatorButtons = document.querySelectorAll(".operator");
         this.eraseButton = document.querySelector(".erase");
         this.clearEntryButton = document.querySelector(".clear-entry");
 
         this.numberInput = this.numberInput.bind(this);
+        this.operatorInput = this.operatorInput.bind(this);
         this.erase = this.erase.bind(this);
         this.clearEntry = this.clearEntry.bind(this);
 
         this.numberButtons.forEach((button) =>
             button.addEventListener("click", this.numberInput)
         );
+        this.operatorButtons.forEach((button) =>
+            button.addEventListener("click", this.operatorInput)
+        );
         this.eraseButton.addEventListener("click", this.erase);
         this.clearEntryButton.addEventListener("click", this.clearEntry);
     }
 
     numberInput(event) {
-        this.currentInput += event.currentTarget.dataset.number;
-        this.logCurrentInput();
+        const currentInput = event.currentTarget.dataset.number;
 
+        // If the array is empty or if the last array item is an operator, push the digit to the array
+        if (this.isInputsEmpty() || this.isLastInputOperator()) {
+            this.inputs.push(currentInput);
+            console.log(this.inputs);
+            return;
+        }
         // If the last array item is a number, append the digit to it
-        // If the last array item is an operator, push the digit to the array
+        this.appendDigit(currentInput);
+        console.log(this.inputs);
+    }
+
+    isInputsEmpty() {
+        return !Boolean(this.inputs.length);
+    }
+    isLastInputNumeric() {
+        const lastInput = this.getLastInput();
+        return !isNaN(lastInput);
+    }
+    isLastInputOperator() {
+        const lastInput = this.getLastInput();
+        const regexp = new RegExp(/[/*\-+]/g);
+        return regexp.test(lastInput);
+    }
+    getLastInput() {
+        const lastIndex = this.inputs.length - 1;
+        const lastInput = this.inputs[lastIndex];
+        return lastInput;
+    }
+    setLastInput(value) {
+        const lastIndex = this.inputs.length - 1;
+        this.inputs[lastIndex] = value;
+    }
+    appendDigit(digit) {
+        const lastIndex = this.inputs.length - 1;
+        this.inputs[lastIndex] += digit;
     }
 
     operatorInput(event) {
-        this.input = event.currentTarget.dataset.operation;
-        console.log(input);
+        this.currentInput = event.currentTarget.dataset.operation;
+
         // If the array is empty, don't do anything
+        if (this.isInputsEmpty()) return;
         // If the last array item is a number, push the operator to the array
+        if (this.isLastInputNumeric()) {
+            this.inputs.push(this.currentInput);
+        }
         // If the last array items is an operator, substitute it with the new operator
+        if (this.isLastInputOperator()) {
+            this.setLastInput(this.currentInput);
+        }
+        console.log(this.inputs);
     }
 
     equals() {
