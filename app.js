55338eeffbd0aed288143224e04cb2c1761007ca
MadMaia3D <33674747+MadMaia3D@users.noreply.github.com>
2023-04-17 15:31:24 -0300

    add: operator buttons functionality

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 class Calculator {
     constructor() {
         this.currentInput = "";
-        this.lastInput = "";
+        this.previousInput = "";
         this.currentOperator = "";
 
         this.querySelectDomElements();
@@ -17,6 +17,7 @@ class Calculator {
 
         this.numberButtons = document.querySelectorAll(".number");
         this.separatorButton = document.querySelector(".separator");
+        this.operatorButtons = document.querySelectorAll(".operator");
         this.clearButton = document.querySelector(".function.clear");
         this.eraseButton = document.querySelector(".function.erase");
     }
@@ -27,16 +28,20 @@ class Calculator {
         this.clearAll = this.clearAll.bind(this);
         this.updateDisplay = this.updateDisplay.bind(this);
         this.eraseLastDigit = this.eraseLastDigit.bind(this);
+        this.inputOperator = this.inputOperator.bind(this);
     }
 
     addEventListenerToButtons() {
-        this.numberButtons.forEach((number) => {
-            number.addEventListener("click", this.insertNumber);
-        });
-
         this.separatorButton.addEventListener("click", this.insertSeparator);
         this.clearButton.addEventListener("click", this.clearAll);
         this.eraseButton.addEventListener("click", this.eraseLastDigit);
+
+        this.numberButtons.forEach((button) => {
+            button.addEventListener("click", this.insertNumber);
+        });
+        this.operatorButtons.forEach((button) => {
+            button.addEventListener("click", this.inputOperator);
+        });
     }
 
     insertNumber(event) {
@@ -56,11 +61,30 @@ class Calculator {
         this.updateDisplay();
     }
 
+    inputOperator(event) {
+        const operator = event.currentTarget.dataset.operation;
+        this.currentOperator = operator;
+
+        const formattedInput = Number(this.currentInput).toString();
+        let displayValue = `${formattedInput}${operator}`;
+
+        const divisionSymbol = "\u{00F7}";
+        const multiplicationSymbol = "\u{00D7}";
+
+        displayValue = displayValue.replace(/\//g, divisionSymbol);
+        displayValue = displayValue.replace(/\*/g, multiplicationSymbol);
+
+        this.previousInput = displayValue;
+        this.currentInput = "";
+        this.updateDisplay();
+    }
+
     updateDisplay() {
         this.displayBottom.textContent = this.currentInput;
         if (this.currentInput === "") {
             this.displayBottom.textContent = "0";
         }
+        this.displayTop.textContent = this.previousInput;
     }
 
     eraseLastDigit() {
@@ -75,7 +99,8 @@ class Calculator {
 
     clearAll() {
         this.currentInput = "";
-        this.lastInput = "";
+        this.previousInput = "";
+        this.currentOperator = "";
         this.updateDisplay();
     }
 }
