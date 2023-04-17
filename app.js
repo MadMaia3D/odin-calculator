3407292c385f2a5233a0a210f761c5ca1ef6b24d
MadMaia3D <33674747+MadMaia3D@users.noreply.github.com>
2023-04-17 15:03:07 -0300

    add: separator and leading zero logic

--- app.js
+++ app.js
@@ -2,32 +2,52 @@ class Calculator {
     constructor() {
         this.currentInput = "";
         this.lastInput = "";
+        this.currentOperator = "";
 
         this.displayTop = document.querySelector(".display .top-row");
         this.displayBottom = document.querySelector(".display .bottom-row");
 
-        this.numberButtons = document.querySelectorAll(".number");
         this.clearButton = document.querySelector(".function.clear");
+        this.numberButtons = document.querySelectorAll(".number");
+        this.separatorButton = document.querySelector(".separator");
 
-        this.inputNumber = this.inputNumber.bind(this);
+        this.insertNumber = this.insertNumber.bind(this);
+        this.insertSeparator = this.insertSeparator.bind(this);
         this.clearAll = this.clearAll.bind(this);
         this.updateDisplay = this.updateDisplay.bind(this);
 
         this.numberButtons.forEach((number) => {
-            number.addEventListener("click", this.inputNumber);
+            number.addEventListener("click", this.insertNumber);
         });
+        this.separatorButton.addEventListener("click", this.insertSeparator);
 
         this.clearButton.addEventListener("click", this.clearAll);
+
+        this.clearAll();
     }
 
-    inputNumber(event) {
+    insertNumber(event) {
+        const pressedNumber = event.currentTarget.dataset.number;
+        if (this.currentInput === "" && pressedNumber === "0") return;
+
         if (this.currentInput.length >= 17) return;
-        this.currentInput += event.currentTarget.dataset.number;
+        this.currentInput += pressedNumber;
+
+        this.updateDisplay();
+    }
+
+    insertSeparator() {
+        if (this.currentInput.includes(".")) return;
+        if (this.currentInput === "") this.currentInput += "0";
+        this.currentInput += ".";
         this.updateDisplay();
     }
 
     updateDisplay() {
         this.displayBottom.textContent = this.currentInput;
+        if (this.currentInput === "") {
+            this.displayBottom.textContent = "0";
+        }
     }
 
     clearAll() {
